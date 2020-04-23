37b17e4edeeb68ee39672fda24982cc596be5665
Balachandar Gopalan <ergbalachandar@gmail.com>
2020-04-23 19:23:56 +0530

    creating entity for table exchange_value

---
+++ ForexService/src/main/java/com/microservice/forexservice/entity/ExchangeValue.java
@@ -0,0 +1,30 @@
+package com.microservice.forexservice.entity;
+
+import java.math.BigDecimal;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "EXCHANGE_VALUE")
+public class ExchangeValue {
+
+	@Id
+	private Long id;
+
+	@Column(name = "currency_from")
+	private String from;
+
+	@Column(name = "currency_to")
+	private String to;
+
+	private BigDecimal conversionMultiple;
+	
+	private int port;
+
+}
