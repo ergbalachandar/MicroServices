9f0f5574a5523af84cf9b6bee818c4b2bba6e5f0
Balachandar Gopalan <ergbalachandar@gmail.com>
2020-04-23 20:18:46 +0530

    getting port number from environment variable

--- ForexService/src/main/java/com/microservice/forexservice/controller/ForexServiceController.java
+++ ForexService/src/main/java/com/microservice/forexservice/controller/ForexServiceController.java
@@ -1,6 +1,7 @@
 package com.microservice.forexservice.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,12 +16,16 @@ public class ForexServiceController {
 	@Autowired
 	ExchangeValueService exchangeValueService;
 
+	@Autowired
+	private Environment environment;
+
 	@GetMapping("/currency-exchange/from/{from}/to/{to}")
 	public ExchangeValueResponseDto retrieveExchangeValue(@PathVariable String from, @PathVariable String to) {
-		
+
 		ExchangeValueResponseDto exchangeValueResponseDto = new ExchangeValueResponseDto();
-        ExchangeValueUtil.mapExchangeValueResponse(exchangeValueService.retrieveExchangeValue(from, to),
+		ExchangeValueUtil.mapExchangeValueResponse(exchangeValueService.retrieveExchangeValue(from, to),
 				exchangeValueResponseDto);
+		exchangeValueResponseDto.setPort(Integer.valueOf(environment.getProperty("local.server.port")));
 
 		return exchangeValueResponseDto;
 	}
