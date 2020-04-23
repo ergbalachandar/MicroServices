86804be4c1443a25761fd936fc90035e4d68f530
Balachandar Gopalan <ergbalachandar@gmail.com>
2020-04-23 20:08:16 +0530

    adding service annotation

--- ForexService/src/main/java/com/microservice/forexservice/service/ExchangeValueService.java
+++ ForexService/src/main/java/com/microservice/forexservice/service/ExchangeValueService.java
@@ -1,10 +1,12 @@
 package com.microservice.forexservice.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.microservice.forexservice.entity.ExchangeValue;
 import com.microservice.forexservice.interfaces.ExchangeValueRepoInterface;
 
+@Service
 public class ExchangeValueService {
 	
 	@Autowired
