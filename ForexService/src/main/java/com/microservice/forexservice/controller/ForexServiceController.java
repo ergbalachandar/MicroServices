8553ae1bd7519cb6c634311e23ae8cd2911ffaf4
Balachandar Gopalan <ergbalachandar@gmail.com>
2020-04-23 20:14:03 +0530

    adding RestController annotation

--- ForexService/src/main/java/com/microservice/forexservice/controller/ForexServiceController.java
+++ ForexService/src/main/java/com/microservice/forexservice/controller/ForexServiceController.java
@@ -3,11 +3,13 @@ package com.microservice.forexservice.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.microservice.forexservice.exchangevalueresponsedto.ExchangeValueResponseDto;
 import com.microservice.forexservice.service.ExchangeValueService;
 import com.microservice.forexservice.util.ExchangeValueUtil;
 
+@RestController
 public class ForexServiceController {
 
 	@Autowired
