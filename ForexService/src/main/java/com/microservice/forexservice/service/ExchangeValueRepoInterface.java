1a7e8694527e93bcf63d287657292c495bd9a675
Balachandar Gopalan <ergbalachandar@gmail.com>
2020-04-23 19:31:29 +0530

    creating interface for ExchangeValue Table

---
+++ ForexService/src/main/java/com/microservice/forexservice/service/ExchangeValueRepoInterface.java
@@ -0,0 +1,14 @@
+package com.microservice.forexservice.service;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.microservice.forexservice.entity.ExchangeValue;
+
+public class ExchangeValueRepoInterface {
+
+	public interface ExchangeValueRepository extends JpaRepository<ExchangeValue, Long> {
+		
+		ExchangeValue findByFromAndTo(String from, String to);
+
+	}
+}
