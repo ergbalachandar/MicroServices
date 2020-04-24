925f1f99683dd1a1feaaa822cfa6bb220cfd8d43
Balachandar Gopalan <ergbalachandar@gmail.com>
2020-04-24 12:36:11 +0530

    implementing load balancing without hardcoding the url of Forex service

--- CurrencyExchangeService/src/main/java/com/microservice/currencyexchangeservice/CurrencyExchangeServiceApplication.java
+++ CurrencyExchangeService/src/main/java/com/microservice/currencyexchangeservice/CurrencyExchangeServiceApplication.java
@@ -6,12 +6,13 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
-@EnableFeignClients("com.microservice.currencyexchangeservice.interfaces")
+@EnableFeignClients("com.microservice.currencyexchangeservice")
 @EnableDiscoveryClient
 public class CurrencyExchangeServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CurrencyExchangeServiceApplication.class, args);
 	}
+	
 
 }
--- CurrencyExchangeService/src/main/java/com/microservice/currencyexchangeservice/interfaces/FeignProxyInterface.java
+++ CurrencyExchangeService/src/main/java/com/microservice/currencyexchangeservice/interfaces/FeignProxyInterface.java
@@ -1,12 +1,14 @@
 package com.microservice.currencyexchangeservice.interfaces;
 
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 import com.microservice.currencyexchangeservice.currencyconversionresponsedto.CurrencyConversionResponseDto;
 
-@FeignClient(name="forex-service", url="localhost:8000")
+@FeignClient(name="forex-service")
+@RibbonClient(name="forex-service")
 public interface FeignProxyInterface {
 
 	@GetMapping("/currency-exchange/from/{from}/to/{to}")
